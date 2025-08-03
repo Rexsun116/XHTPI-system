f030087bcf21cbb034cba5d286ab7db564d9c9dc
root <root@localhost.localdomain>
2025-08-03 12:03:28 +0800

    本地修改：更新删除功能及订单列表页面

--- delete_pi.py
+++ delete_pi.py
@@ -1,9 +1,9 @@
 from app import app, db, PI
 with app.app_context():
-    pi = PI.query.filter_by(pi_no='TEST0027').first()
+    pi = PI.query.filter_by(pi_no='Q2507HIG001-1').first()
     if pi:
         db.session.delete(pi)
         db.session.commit()
-        print("PI TEST0027 deleted successfully")
+        print("PI Q2507HIG001-1 deleted successfully")
     else:
-        print("PI TEST0027 not found")
+        print("PI Q2507HIG001-1 not found")
